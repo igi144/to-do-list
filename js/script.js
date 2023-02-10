0d33c2f1770451beef76d3482b6a417a1798089b
Igor Pelczarski <igorpe144@gmail.com>
2023-02-10 15:28:17 +0100

    added new function in js, classes names changed

--- js/script.js
+++ js/script.js
@@ -7,31 +7,29 @@
 
         for (task of tasks) {
 
-            stringList += `
-            
-            <li 
-            class="js__listClass" 
-            >
+            stringList +=
+                `
+            <li class="js__listClass">
 
-             <button class="js-toggleButton">
-            ${task.done ? "✓" : ""}
-            </button>
+              <button class="js-toggleButton">
+              ${task.done ? "✓" : ""}
+              </button>
            
-           <span ${task.done ? " style=\"text-decoration: line-through\"   " : ""}> ${task.content}</span>
+              <span class="${task.done ? " js__content--done " : ""}"> 
+              
+              ${task.content}
+              </span>
            
-           <button class="js-removeButton button">
-           <img src="jpg/pngwing.com.png">
-           </button>
+              <button class="js-removeButton button">
+              <img src="jpg/pngwing.com.png">
+              </button>
              </li>
-             
-            `
+             `
         }
 
         document.querySelector(".js-tasks").innerHTML = stringList;
 
         bindEvents();
-
-
     }
 
     const bindEvents = () => {
@@ -40,7 +38,6 @@
 
         removeButton.forEach((remove, index) => {
             remove.addEventListener("click", () => {
-
                 removeTask(index);
             })
         })
@@ -49,49 +46,40 @@
 
         toggleTask.forEach((toggle, indexTask) => {
             toggle.addEventListener("click", () => {
-
                 scratchTask(indexTask);
             })
         })
-
-
     }
 
-
     const scratchTask = (indexTask) => {
-
         tasks[indexTask].done = !tasks[indexTask].done;
         render();
     }
 
     const removeTask = (index) => {
-
         tasks.splice(index, 1);
         render(index);
     }
 
-    const addNewTask = (jsNewTask) => {
-
+    const addNewTask = (newTask) => {
         tasks.push({
-
-            content: jsNewTask
-
+            content: newTask
         })
 
         render();
-
-
     }
 
     const onFormSubmit = (event) => {
         event.preventDefault();
 
-        const jsNewTask = document.querySelector(".js-newTask").value.trim();
+        const jsNewTask = document.querySelector(".js-newTask");
+        const jsNewTaskWord = jsNewTask.value.trim();
 
-        if (jsNewTask === "")
-            return;
-
-        addNewTask(jsNewTask)
+        if (jsNewTaskWord !== "") {
+            addNewTask(jsNewTaskWord);
+            jsNewTask.value = "";
+        }
+        jsNewTask.focus();
 
     }
 
@@ -99,16 +87,8 @@
         render();
 
         const form = document.querySelector(".js-form")
-
         form.addEventListener("submit", onFormSubmit)
-
     }
 
     init();
-
-
-
-
-
-
 }
